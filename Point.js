82ea59ddb7ac576963c47de3710d5581a32e86c5
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2022-08-05 22:28:38 +0100

    add

--- Point.js
+++ Point.js
@@ -8,6 +8,9 @@ class Point {
       throw `This cordinate is not a point in the curve ${this.x},${this.y}`
     }
   }
+  toString(){
+    return `x${this.x}, y ${this.y}, a ${this.a}, b${thus.b}`
+  }
   equal(other){
     this.#checkIsPoint(other);
     return this.x === other.x && this.y === other.y && this.a === other.a && this.b === other.b
@@ -16,6 +19,21 @@ class Point {
   notEqual(other){
     this.#checkIsPoint(other);
     return this.x != other.x && this.y != other.y && this.a != other.a && this.b != other.b
+  }
+  add(other){
+    this.#checkIsPoint(other);
+    if(this.a != other.a || this.b != other.b){
+      throw `The ${other.toString()},${this.toString()} points are not in the same curve` 
+    }
+    if(!this.x){
+      return other;
+    }
+    if(!other.x){
+      return this
+    }
+    if(this.x === other.x && this.y != other.y){
+      return new Point(null,null,this.a,this.b)
+    }
   }
 
   #checkIsPoint(point){
@@ -29,4 +47,4 @@ let p1 = new Point(-1, -1, 5, 7);
 // let p2 = new Point(-1, -2, 5, 7) Gives an error that the cordinates is not a curve
 // let p3 = new Point(2, 4, 5, 7);  Gives an error that the cordinates is not a curve
 let p4 = new Point(18, 77, 5, 7);
-let p5 = new Point(5, 7, 5, 7);
+// let p5 = new Point(5, 7, 5, 7);
